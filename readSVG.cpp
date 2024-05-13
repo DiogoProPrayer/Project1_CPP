279c324d97cb350087c09fa232987efd93c0d69e
DiogoProPrayer <diogoalexandrepinheiro@gmail.com>
2024-05-13 01:55:45 +0100

    Can now read all types of gemometrical forms

--- readSVG.cpp
+++ readSVG.cpp
@@ -31,14 +31,14 @@ namespace svg
 
     void readElements(XMLElement *root, XMLElement *elem,vector<SVGElement *>& svg_elements, vector<string> transformations = {}){
         
-        
+
         if (elem->Name()=="ellipse"){
             //Define an Ellipse
             Point p = {elem->IntAttribute("cx"), elem->IntAttribute("cy")};
             Point radius = {elem->IntAttribute("rx"), elem->IntAttribute("ry")};
             Ellipse objeto(parse_color(elem->Attribute("fill")), p, radius);
 
-            //Check if any transformation is aplied, and transform if needed
+            //Check if any transformation is applied, and transform if needed
             if (elem->Attribute("transform")!=0)
             {
                 if (elem->Attribute("transform-origin")!=0)
@@ -57,7 +57,7 @@ namespace svg
             Point p = {elem->IntAttribute("cx"), elem->IntAttribute("cy")};
             Circle objeto(parse_color(elem->Attribute("fill")), p, elem->IntAttribute("r"));
 
-            //Check if any transformation is aplied, and transform if needed
+            //Check if any transformation is applied, and transform if needed
             if (elem->Attribute("transform")!=0)
             {
                 if (elem->Attribute("transform-origin")!=0)
@@ -78,7 +78,111 @@ namespace svg
             Point p = {elem->IntAttribute("x"), elem->IntAttribute("y")};
             Rectangle objeto(parse_color(string(elem->Attribute("fill"))), elem->IntAttribute("height"), elem->IntAttribute("width"), p);
 
-            //Check if any transformation is aplied, and transform if needed
+            //Check if any transformation is applied, and transform if needed
+            if (elem->Attribute("transform")!=0)
+            {
+                if (elem->Attribute("transform-origin")!=0)
+                {
+                    objeto.transform(elem->Attribute("transform"),elem->Attribute("transform-origin"));
+                } else{
+                    objeto.transform(elem->Attribute("transform"));
+                }
+            }
+
+            //Apply group transformations (to be done)
+
+        }
+         else if (elem->Name()=="polyline")
+        {
+            //Define a Polyline
+            string points = elem->Attribute("points");
+            points += ' ';
+            vector <Point> cluster = {};
+            Point ponto = {};
+            string cache = "";
+            for(char c: points){
+                if (c==',')
+                {
+                    ponto.x = stoi(cache);
+                    cache = "";
+                } else if ((c==' ') && (cache!=""))
+                {
+                    ponto.y = stoi(cache);
+                    cache = "";
+                    cluster.push_back(ponto);
+                    ponto.x = 0;
+                    ponto.y = 0;
+                } else if ((c>='0')&&(c<='9'))
+                {
+                    cache.push_back(c);
+                }
+            }
+            Polyline objeto(parse_color(elem->Attribute("stroke")), cluster);
+            
+
+            //Check if any transformation is applied, and transform if needed
+            if (elem->Attribute("transform")!=0)
+            {
+                if (elem->Attribute("transform-origin")!=0)
+                {
+                    objeto.transform(elem->Attribute("transform"),elem->Attribute("transform-origin"));
+                } else{
+                    objeto.transform(elem->Attribute("transform"));
+                }
+            }
+
+            //Apply group transformations (to be done)
+
+        }else if (elem->Name()=="line")
+        {
+            //Define a Line
+            Point start = {elem->IntAttribute("x1"),elem->IntAttribute("y1")};
+            Point end = {elem->IntAttribute("x2"),elem->IntAttribute("y2")};
+            Line objeto(parse_color(elem->Attribute("stroke")), start, end);
+            
+
+            //Check if any transformation is applied, and transform if needed
+            if (elem->Attribute("transform")!=0)
+            {
+                if (elem->Attribute("transform-origin")!=0)
+                {
+                    objeto.transform(elem->Attribute("transform"),elem->Attribute("transform-origin"));
+                } else{
+                    objeto.transform(elem->Attribute("transform"));
+                }
+            }
+
+            //Apply group transformations (to be done)
+
+        }  else if (elem->Name()=="polygon")
+        {
+            //Define a Polygon
+            string points = elem->Attribute("points");
+            points += ' ';
+            vector <Point> poly = {};
+            Point ponto = {};
+            string cache = "";
+            for(char c: points){
+                if (c==',')
+                {
+                    ponto.x = stoi(cache);
+                    cache = "";
+                } else if ((c==' ') && (cache!=""))
+                {
+                    ponto.y = stoi(cache);
+                    cache = "";
+                    poly.push_back(ponto);
+                    ponto.x = 0;
+                    ponto.y = 0;
+                } else if ((c>='0')&&(c<='9'))
+                {
+                    cache.push_back(c);
+                }
+            }
+            Polygon objeto(parse_color(elem->Attribute("fill")), poly);
+            
+
+            //Check if any transformation is applied, and transform if needed
             if (elem->Attribute("transform")!=0)
             {
                 if (elem->Attribute("transform-origin")!=0)
@@ -96,13 +200,13 @@ namespace svg
 
 
 
+
+
+
         for (XMLElement *child = elem->FirstChildElement(); child != nullptr; child = child->NextSiblingElement())
     {
         readElements(root, child, svg_elements, transformations);   
     }
-
-
-
 
 
     }
